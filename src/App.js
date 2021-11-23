e66375f9b37f17bca1162b8a78affdc9af7b6777
sugarmouse <tanghao19950911@gmail.com>
2021-11-24 00:06:26 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import Header from './components/header';
+import Footer from './components/footer';
+import Loading from './components/Loading';
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+
+const Home = React.lazy(() => import('./pages/Home.js'))
+const History = React.lazy(() => import('./pages/History.js'))
+const About = React.lazy(() => import('./pages/About.js'))
+const Login = React.lazy(() => import('./pages/Login.js'))
+const Register = React.lazy(() => import('./pages/Register.js'))
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/history' element={<History />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+          </Routes>
+        </Suspense>
+      </main>
+      <Footer />
+    </>
   );
 }
-
 export default App;
