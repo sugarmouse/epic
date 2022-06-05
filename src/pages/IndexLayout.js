70e26f255c60a2d31146b48883bdbedee38d805f
sugarmouse <tanghao19950911@gmail.com>
2022-06-06 01:41:51 +0800

    弃用视频动态背景

--- src/pages/IndexLayout.js
+++ src/pages/IndexLayout.js
@@ -1,7 +1,5 @@
 import {Outlet} from 'react-router-dom'
 import {Layout} from '../components/Layout'
-import webm from '../assets/home-page-background.webm'
-import backgroundImg from '../assets/home-page-background.png'
 import {Logo} from '../components/Logo'
 import styled from 'styled-components'
 
@@ -19,10 +17,6 @@ export const IndexLayout = () => {
   return (
     <Layout backgroundColor={'rgba(0,0,0,0.4)'} isShowHeader={false} isShowFooter={false}>
       <Div>
-        <video controls={false} playsInline autoPlay muted loop
-               poster={backgroundImg} id="bgvid">
-          <source src={webm} type="video/webm"/>
-        </video>
         <div className='content'>
           <div className='basic-content'>
             <Logo size={'10rem'}/>
